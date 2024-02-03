640f03bd13520b7501dfaebd8cc5cfc084991fa0
Paras Jain <paras06@gmail.com>
2024-02-03 08:46:15 +0000

    WEEK 4 - ASSIGNMENT 4 SPLITING AND JOINING

--- Assignments/WEEK 4/ASSIGNMENT 4/q1.py
+++ Assignments/WEEK 4/ASSIGNMENT 4/q1.py
@@ -1,26 +1,18 @@
 """
-Ask a string from user. If the length of string is odd, then change all the
-capital letters to small and vice versa, but if the length of string is even,
-reverse the string. Do this using a function and return the output.
-
-
+Ask a string from user. If the length of string is odd, 
+then change all the capital letters to small and vice versa, 
+but if the length of string is even, reverse the string. 
+Do this using a function and return the output
 """
 
 
-def changeString(str):
-    result = ""
-    if len(str) % 2 != 0:
-        for i in str:
-            if i.islower():
-                result = result + i.upper()
-            else:
-                result = result + i.lower()
-        return result
-    else:
-        result = str[::-1]
-    return result
+def changeString(string: str) -> str:
+    if len(string) % 2 == 0:
+        return string[::-1]
+    return string.swapcase()
 
 
-user_input = input("Enter a string: ")
-print(changeString(user_input))
-# str.tol
+r = changeString("a432432eradADAWplaAne")
+print(r)
+r = changeString("a432432eradADAWplaAn")
+print(r)
